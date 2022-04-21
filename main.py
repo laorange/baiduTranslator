cbabda3e2e4fcf4718a4aa174b99dd705135f183
辣橙 <laorange6666@qq.com>
2022-04-21 18:12:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from baiduTranslator import rewrite
 if __name__ == '__main__':
     with open('text.txt', encoding='utf-8') as file:
         text = file.read()
+
     result = rewrite(text)
     print("原文:", text, sep='\n')
     print("-" * 10)
     print("处理后:", result, sep="\n")
+
+    with open("output.txt", 'wt', encoding='utf-8') as file:
+        file.write(result)
