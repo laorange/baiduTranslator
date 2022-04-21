fea9381a3c181f9a25d9b9b19cc9078f4601d522
辣橙 <laorange6666@qq.com>
2022-04-21 13:22:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ if __name__ == '__main__':
     with open('text.txt', encoding='utf-8') as file:
         text = file.read()
     result = rewrite(text)
-    print(result)
+    print("原文:", text, sep='\n')
+    print("-" * 10)
+    print("处理后:", result, sep="\n")
